4bfd25d2108f2da4f3b122ab85c1de45b7c75eb2
Kavya Mahapatro <delishamahapatro@gmail.com>
2026-09-09 09:52:59 +0530

    Time: 1 ms (96.93%) | Memory: 47.3 MB (59.65%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        
+        // I create a dummy node before the head to handle deletion of the first node easily. I check the next node's value instead of the current node's value. If it matches val, I bypass that node using curr.next = curr.next.next. Otherwise I move forward; the solution is O(n) time and O(1) space
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode curr = dummy;
+
+        while (curr.next != null) {
+            if (curr.next.val == val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+
+        return dummy.next;
+
+    }
+}
